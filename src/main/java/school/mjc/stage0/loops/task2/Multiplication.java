9ea826b5a3a6b8c646d0ca06c4442f11d7345c7e
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-20 18:13:37 +0400

    stage0Module4loopsTask2

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int N=0 ;
+        int i = 1;
+        while (i <= multiplyByAndToInclusive) {
+            System.out.println(N +  i + N * i);
+            i++;
+        }
     }
 }

88ed14cd4fb39c095afc8c58ef8a7fe1fb872575
Isabella Karibian <isabellakaribian@gmail.com>
2023-07-20 18:13:23 +0400

    stage0Module4loopsTask2

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        int cnt = 0;
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                cnt++;
+            }
+            power = power >> 1;
+            System.out.println(cnt);
+        }
     }
 }

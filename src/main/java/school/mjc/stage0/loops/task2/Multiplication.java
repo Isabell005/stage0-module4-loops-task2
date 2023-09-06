3e8614c6347453b9db1bf5b3471187f2789cf2d4
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-06 12:33:22 +0400

    stage0Module4loopsTask2

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -1,13 +1,17 @@
 package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
+
+    public static void main(String[] args) {
+        Multiplication m = new Multiplication();
+        m.printMultiplied(5); // Prints multiplication row up to 5
+    }
+
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int a=0 ;
-        int i = 1;
-        while (i <= multiplyByAndToInclusive) {
-            System.out.println(a* i);
-            i++;
-            a++;
+        int number = (multiplyByAndToInclusive < 0) ? multiplyByAndToInclusive : 0;
+        while (number <= multiplyByAndToInclusive) {
+            System.out.println(multiplyByAndToInclusive * number);
+            number++;
         }
     }
 }

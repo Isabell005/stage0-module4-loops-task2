0fd57c6f07062a6282fbdebf56b302357f92cc9b
Isabella Karibian <isabellakaribian@gmail.com>
2023-09-06 12:28:24 +0400

    stage0Module4loopsTask2

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -1,13 +1,30 @@
 package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
+    public static void main(String[] args) {
+        FactorialNumbers fn = new FactorialNumbers();
+        fn.printFactorialRow(10); // Prints factorial row up to 10
+    }
+
     public void printFactorialRow(int printToInclusive) {
-        int fact = 1;
-        int i = 1;
-        while (i <= printToInclusive) {
-            fact = fact * i;
-            System.out.println(fact);
+        int number = 0;
+        while (number <= printToInclusive) {
+            System.out.println(calculateFactorial(number));
+            number++;
+        }
+    }
+
+    public long calculateFactorial(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+
+        long factorial = 1;
+        int i = 2;
+        while (i <= number) {
+            factorial = factorial * i;
             i++;
         }
+        return factorial;
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,11 +2,12 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int N=0 ;
+        int a=0 ;
         int i = 1;
         while (i <= multiplyByAndToInclusive) {
-            System.out.println(N +  i + N * i);
+            System.out.println(a* i);
             i++;
+            a++;
         }
     }
 }
